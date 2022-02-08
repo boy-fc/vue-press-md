246487c634aabd33d53a7341364d0ffca4c00ab2
orangewind <954506111@qq.com>
2022-02-08 23:20:02 +0800

    文件配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,6 +12,34 @@ module.exports = {
         children: [ // 下级列表
           'typeScript/1_安装',
           'typeScript/2_基本类型',
+          'typeScript/3_接口',
+          'typeScript/4_类',
+          'typeScript/5_函数',
+          'typeScript/6_泛型',
+          'typeScript/7_其他',
+        ]
+      },
+      {
+        title: 'Vue3快速上手', // 标题
+        collapsable: false, // 下级列表不可折叠
+        children: [ // 下级列表
+          'vue3.0/1_创建vue3项目',
+          'vue3.0/2_Composition API',
+          'vue3.0/3_Composition API(其它)',
+          'vue3.0/4_手写组合API',
+          'vue3.0/5_新组件',
+        ]
+      },
+      {
+        title: 'interview', // 标题
+        collapsable: false, // 下级列表不可折叠
+        children: [ // 下级列表
+          'interview/1_HTML、CSS相关',
+          'interview/2_JS相关',
+          'interview/3_Vue2',
+          'interview/4_网络安全、HTTP协议',
+          'interview/5_前端工程化',
+          'interview/6_前端性能优化',
         ]
       },
     ]
---
+++ docs/vue3.0/1_创建vue3项目.md
@@ -0,0 +1,25 @@
+### 创建vue3项目
+
+#### vue-cli创建
+
+```js
+// 安装或者升级
+npm install -g @vue/cli
+// 保证vue cli 版本在 4.5.0 以上
+vue --version
+// 创建项目
+vue create my-project
+```
+
+#### vite创建
+
+```js
+// 全局安装create-vite-app
+npm i -g create-vite-app
+// 创建项目目录
+create-vite-app 项目名
+// 运行
+cd 项目名
+npm install
+npm run dev
+```
